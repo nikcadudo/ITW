9f2836c5c727cb1bccc556f9bfd84e377a1a7180
nikcadudo <33050372+nikcadudo@users.noreply.github.com>
2020-04-20 09:57:01 +0200

    now working hamburger menu when in mobile mode, then classical navbar for other devices

--- main.js
+++ main.js
@@ -16,10 +16,18 @@ function changeProject(e, project){
 }
 
 function showMenu(){
-    var x = document.getElementsByClassName("navbar-nav");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+    document.getElementById("myDropdown").classList.toggle("show");
 }
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
